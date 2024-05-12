f06dfaca11ec52a6790b854306cda087aacf174f
sorolleta <sorollayeray@gmail.com>
2024-05-12 21:23:18 +0200

    Version 2

--- Prueba2/src/Pruebas/Prueba.java
+++ Prueba2/src/Pruebas/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("jaygsdua");
+		System.out.println("iaUghsdiads");
 	}
 
 }
